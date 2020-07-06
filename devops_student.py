255c5e812f27b5cd21efda56d128f20ab4304e12
marwai <mtse@spartaglobal.com>
2020-07-07 00:19:12 +0100

    abstraction

--- devops_student.py
+++ devops_student.py
@@ -1,20 +1,54 @@
-# imports class
-from student_data import Student
+# # # imports class
+# # from student_data import Student
+# #
+# # # inherits first_name, last_name attributes
+# class Devops(Student):
+#     def __init__(self, id,first_name, last_name,skills,age):
+#         super().__init__(first_name, last_name, age)
+#         self.skills = skills
+#         self.id = id
+#
+#
+#     def spartan_summary(self):
+#         print("ID:",self.id, "\nName:",self.first_name,"\nSurname:",self.last_name,"\nSkills:", self.skills )
+#     def spartan_skills(self):
+#         print("Skills:", self.skills)
+# #
+# student_one = Devops("1241","Marcus","Tse", "SQL")
+# # student_one.full_name() # method is hidden to the user
 
-# inherits first_name, last_name attributes
-class Devops(Student):
-    def __init__(self, id,first_name, last_name,skills, ):
-        super().__init__(first_name, last_name)
-        self.skills = skills
-        self.id = id
 
-    0
-    def spartan_summary(self):
-        print("ID:",self.id, "\nName:",self.first_name,"\nSurname:",self.last_name,"\nSkills:", self.skills )
-    def spartan_skills(self):
-        print("Skills:", self.skills)
 
-student_one = Devops("1241","Marcus","Tse", "SQL")
-# student_one.full_name()
+
+#
+#
 # student_one.spartan_skills()
-student_one.spartan_summary()
+# student_one.spartan_summary()
+
+# Imports everything (*) from student_data
+from student_data import *
+
+# The subclass devops has student as its base class
+class Devops(Student): # The class inherits all the characteristics from student_data
+    def __init__(self, name, age, course, skills):
+        super().__init__(name, age, course)
+        self.skills = skills # unique to devops only
+
+    # Here in the student class there is a different fuctionality
+    def power(self):
+        print("remember to use power words to sell yourself")
+
+
+#Recalls the instance of the class
+
+# instance of method has been named from Devops
+b =  Devops("bob", "25", "Computer Science", "SQL, Python and AWS\n")
+# instance of Student
+a = Student("bob", "25", "Computer Science")
+
+#Both produce different outputs because the behaviour in the parent class and child class differs
+a.power()
+# a.graduate()
+print("___")
+b.power()
+# b.graduate()
--- student_data.py
+++ student_data.py
@@ -1,9 +1,65 @@
+# # class Student:
+# #     def info(self):
+# #         pass
+# # student_details = Student()
+# # Currently not abstract
+# # student_details.info()
+#
+# ##This is abstract
+# from abc import ABC, abstractmethod
+# class Student(ABC):
+#     @abstractmethod
+#     def info(self):
+#         pass
+#
+# # Currently not abstract
+# student_details.info()
+#
+# ##part 2
+# class Adults(Student): # needs to be define other this will be abstract
+#     def info(self):
+#         print("hi spartan")
+#
+# # stu = Student()
+# human = Adults() # Can't instantiate abstract class Student with abstract methods info
+# # stu.info()
+
+
+
+# Parent class
+
+# class Student:
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+#     # when a method has a body, it is a normal method
+#     def full_name(self):
+#         print(self.first_name, self.last_name)
+#     # a method with no body is called declared meaning the definition is blank
+#     # these are known as abstract methods
+#     def age(self):
+#         pass
+    # a with class with an abstract method is also an abstract class
+from abc import ABC, abstractmethod
+# The super class, Student is the class that other classes inherits from
 class Student:
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+    # method (behaviour) initialises the variables
+    def __init__(self, name, age, course): # (name, age, course attributes are defined)
+        self.name = name
+        self.__age = age
+        self.course = course
 
-    def full_name(self):
-        print(self.first_name, self.last_name)
+    # Details of the student
+    def details(self):
+        # basic print functions
+        print("Name: " + self.name)
+        print("Age: " + self.__age)
+        print("University course: " + self.course)
 
-
+    def graduate(self):
+        print("I graduated from university recently")
+    # @abstract methods allows us to declare an method with no body in the parent class, keeping it hidden
+    @abstractmethod
+    def power(self):
+         pass
